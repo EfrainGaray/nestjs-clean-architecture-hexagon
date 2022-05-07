8bf17cb6b6ad16169af57d71042653a36aa70e47
Efrain Garay <egaray7@gmail.com>
2022-05-07 13:45:00 -0400

    feat(): init config development

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { AppService } from './app.service';
+
+function setupSwagger(app: INestApplication): void {
+  const documentBuilder = new DocumentBuilder()
+    .setTitle('Nest.js example Hexagonal Architecture')
+    .setDescription('This is example for nest.js')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentBuilder);
+  SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  setupSwagger(app);
+  await app.listen(AppService.port());
 }
 bootstrap();
